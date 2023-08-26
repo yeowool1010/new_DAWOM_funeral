3cd010934ab5bdd200ee2534adc6b1aaf0942bc5
yeowool1010 <qyoong3579@gmail.com>
2023-08-26 19:16:38 +0900

    💄 footer 생성

---
+++ new_dawom/src/component/main-/Footer.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import SocialIcon from './social-icons'
+import siteMetadata from '../../data/siteMetadata'
+
+export function Footer() {
+  return (
+    <footer>
+      <div className="flex flex-col items-center mt-16">
+        <div className="flex mb-3 space-x-4">
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
+          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
+          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+        </div>
+        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>{'박철진'}</div>
+          <div>{` • `}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <Link href="/">{'mail@gmail.com'}</Link>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">DAWOM funeral service company</Link>
+        </div>
+      </div>
+    </footer>
+  )
+}
