51a4dc6ecdecef5a05b367e146d704c2dc0155e7
yeowool1010 <qyoong3579@gmail.com>
2023-08-26 23:38:05 +0900

    💄 메인페이지 상단 임시 이미지 삽입

--- new_dawom/src/component/main-/HomeComponent.tsx
+++ new_dawom/src/component/main-/HomeComponent.tsx
@@ -4,10 +4,13 @@ import Image from 'next/image'
 export function HomeComponent() {
   return (
     <MainContainer>
-      <div className="relative w-full h-full overflow-hidden bg-center bg-no-repeat bg-cover">
-        <Image src="/img/main/visual01.jpg" alt="Description" width={500} height={300} />
+      <div className="absolute top-0 w-full h-full overflow-hidden bg-center bg-no-repeat bg-cover">
+        <Image src="/img/main/visual01.jpg" alt="Description" layout="responsive" width={1000} height={200} />
       </div>
 
+      <div className="pt-64 text-4xl text-right text-white">
+        <p>안녕</p>
+      </div>
       <h2 className={`mb-3 text-2xl font-semibold`}>메인페이지</h2>
     </MainContainer>
   )
