e2a1b183b48c333cf3be4abf3cdd6082936d0d61
yeowool1010 <qyoong3579@gmail.com>
2023-09-03 00:13:21 +0900

    ✨ recoil 세팅 및 메뉴 on,off 토글 상태 생성

--- new_dawom/src/app/layout.tsx
+++ new_dawom/src/app/layout.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import '../styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Header, CallNav2, Footer } from '../component/main-'
+import { RecoilRoot } from 'recoil'
 import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,13 +16,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="kr">
-      <body className={inter.className}>
-        <Header/>
-        <CallNav2 />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <RecoilRoot>
+      <html lang="kr">
+        <body className={inter.className}>
+          <Header />
+          <CallNav2 />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </RecoilRoot>
   )
 }
