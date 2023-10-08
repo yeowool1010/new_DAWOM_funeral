0e17e30f6424572178fdaf8941b2d83a2040b0a1
yeowool1010 <qyoong3579@gmail.com>
2023-10-08 20:07:54 +0900

    💄 [main] 중앙 회사소개 문구 및 아이콘 변경

--- new_dawom/src/component/main-/HomeComponent.tsx
+++ new_dawom/src/component/main-/HomeComponent.tsx
@@ -8,40 +8,33 @@ export function HomeComponent() {
         <Image src="/img/main/visual01.jpg" alt="Description" layout="responsive" width={1000} height={200} />
       </div>
       <MainInnerContainer>
-        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Work fast from anywhere</h5>
+        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">회사소개</h5>
         <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
-          Stay up to date and move work forward with Flowbite on iOS & Android. Download the app today.
+          다움상조는 유가족의 편에서 일하고 있습니다. 장례식을 마치고 결제하는 정직한 후불 결제 시스템
         </p>
         <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
           <a
             href="#"
             className="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
           >
-            <svg
-              className="mr-3 w-7 h-7"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="apple"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="mr-3 w-7 h-7">
               <path
-                fill="currentColor"
-                d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
-              ></path>
+                fill-rule="evenodd"
+                d="M4.5 2.25a.75.75 0 000 1.5v16.5h-.75a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5h-.75V3.75a.75.75 0 000-1.5h-15zM9 6a.75.75 0 000 1.5h1.5a.75.75 0 000-1.5H9zm-.75 3.75A.75.75 0 019 9h1.5a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM9 12a.75.75 0 000 1.5h1.5a.75.75 0 000-1.5H9zm3.75-5.25A.75.75 0 0113.5 6H15a.75.75 0 010 1.5h-1.5a.75.75 0 01-.75-.75zM13.5 9a.75.75 0 000 1.5H15A.75.75 0 0015 9h-1.5zm-.75 3.75a.75.75 0 01.75-.75H15a.75.75 0 010 1.5h-1.5a.75.75 0 01-.75-.75zM9 19.5v-2.25a.75.75 0 01.75-.75h4.5a.75.75 0 01.75.75v2.25a.75.75 0 01-.75.75h-4.5A.75.75 0 019 19.5z"
+                clip-rule="evenodd"
+              />
             </svg>
+
             <div className="text-left">
-              <div className="mb-1 text-xs">Download on the</div>
-              <div className="-mt-1 font-sans text-sm font-semibold">Mac App Store</div>
+              <div className="mb-1 text-xs"> 다움상조 안내</div>
+              <div className="-mt-1 font-sans text-sm font-semibold">기업 소개 더보기</div>
             </div>
           </a>
           <a
             href="#"
             className="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
           >
-            <svg
+            {/* <svg
               className="mr-3 w-7 h-7"
               aria-hidden="true"
               focusable="false"
@@ -55,10 +48,20 @@ export function HomeComponent() {
                 fill="currentColor"
                 d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"
               ></path>
+            </svg> */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="mr-3 w-7 h-7">
+              <path d="M11.625 16.5a1.875 1.875 0 100-3.75 1.875 1.875 0 000 3.75z" />
+              <path
+                fill-rule="evenodd"
+                d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm6 16.5c.66 0 1.277-.19 1.797-.518l1.048 1.048a.75.75 0 001.06-1.06l-1.047-1.048A3.375 3.375 0 1011.625 18z"
+                clip-rule="evenodd"
+              />
+              <path d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
             </svg>
+
             <div className="text-left">
-              <div className="mb-1 text-xs">Get in on</div>
-              <div className="-mt-1 font-sans text-sm font-semibold">Google Play</div>
+              <div className="mb-1 text-xs">운영방식 소개</div>
+              <div className="-mt-1 font-sans text-sm font-semibold">후불제상품 상세</div>
             </div>
           </a>
         </div>
