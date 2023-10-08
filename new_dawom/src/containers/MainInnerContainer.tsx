357222c52accf5046c53c741d92a02546941a42e
yeowool1010 <qyoong3579@gmail.com>
2023-10-08 20:47:22 +0900

    💄 [main] MainInnerContainer전체 레이아웃 플렉스, 센터 css수정

--- new_dawom/src/containers/MainInnerContainer.tsx
+++ new_dawom/src/containers/MainInnerContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function MainInnerContainer({ children }: Props) {
   return (
-    <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex justify-center w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
       {children}
     </div>
   )
