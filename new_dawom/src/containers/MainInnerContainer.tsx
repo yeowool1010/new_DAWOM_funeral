35b6a8e290f4892a3a5e84fd07b4c6f0c187116a
yeowool1010 <qyoong3579@gmail.com>
2023-10-09 14:49:01 +0900

    💄 [main] 메인 상품 태그 버튼 반응형 css구현

--- new_dawom/src/containers/MainInnerContainer.tsx
+++ new_dawom/src/containers/MainInnerContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function MainInnerContainer({ children }: Props) {
   return (
-    <div className="flex justify-center w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex items-center justify-center w-full p-4 text-center bg-white border border-gray-200 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
       {children}
     </div>
   )
