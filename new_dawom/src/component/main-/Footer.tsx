2f7857fc6958772eae39e9e9a05a7a8242df824f
yeowool1010 <qyoong3579@gmail.com>
2023-10-09 20:41:43 +0900

    💄 [main] footer 반응형 css조정

--- new_dawom/src/component/main-/Footer.tsx
+++ new_dawom/src/component/main-/Footer.tsx
@@ -14,17 +14,27 @@ export function Footer() {
           {/* <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} /> */}
           {/* <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} /> */}
         </div>
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{'다움상조'}</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{`Tel : 032-344-4446 | Fax : 032-341-4447`}</Link>
+        <div className="flex flex-col mb-2 space-x-2 text-sm text-gray-500 App:items-center dark:text-gray-400">
+          <div className="hidden Tab:flex Tab:flex-row">
+            <div>{'다움상조'}</div>
+            <div>&nbsp;{` • `}&nbsp;</div>
+            <div>{`© ${new Date().getFullYear()}`}</div>
+            <div>&nbsp;{` • `}&nbsp;</div>
+            <Link href="/">{`Tel : 032-344-4446 | Fax : 032-341-4447`}</Link>
+          </div>
+
+          <div className="flex flex-row Tab:hidden">
+            <div>{'다움상조'}</div>
+            {/* <div>{` • `}</div> */}
+            <div>{`© ${new Date().getFullYear()}`}</div>
+            {/* <div>{` • `}</div> */}
+          </div>
+          <Link className="Tab:hidden" href="/">{`Tel : 032-344-4446 | Fax : 032-341-4447`}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            주소 : 인천광역시 남동구 구월로 336번길 63-23 | E-mail : yr4417@naver.com
-          </Link>
+        <div className="flex flex-col items-center mb-8 text-sm text-gray-500 Tab:flex-row dark:text-gray-400">
+          <p>주소 : 인천광역시 남동구 구월로 336번길 63-23</p>
+          <p className="hidden Tab:block">&nbsp;|&nbsp;</p>
+          <p>E-mail : yr4417@naver.com</p>
         </div>
       </div>
     </footer>
