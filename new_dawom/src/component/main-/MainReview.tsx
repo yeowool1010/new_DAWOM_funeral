6c23076e8a40b5bdb6924db4cec30684455a2ada
yeowool1010 <qyoong3579@gmail.com>
2023-10-09 21:53:27 +0900

    💄 [main] 리뷰카드 반응형 css 구현

--- new_dawom/src/component/main-/MainReview.tsx
+++ new_dawom/src/component/main-/MainReview.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export function MainReview() {
   return (
-    <div className="flex flex-col w-11/12 p-3 m-3 bg-white border border-gray-200 rounded-lg shadow Tab:flex-row dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex flex-col items-center w-11/12 p-3 m-3 bg-white border border-gray-200 rounded-lg shadow Tab:items-start Tab:flex-row dark:bg-gray-800 dark:border-gray-700">
       <div className="Tab:w-1/3" id="defaultTabContent">
         <div
           className="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800"
@@ -15,7 +15,7 @@ export function MainReview() {
           {/* 긴 글 줄바꿈 되지 않도록 이 줄만 프리티어 적용 ignore 함 */}
           {/* clamp-3은 tailwind plugins으로 세 줄 이상일 경우 ... 처리하는것임*/}
           {/* prettier-ignore */}
-          <p className="w-[250px] clamp-3">
+          <p className="w-[18rem] Tab:w-[11rem] clamp-3">
             몇일전 대전에서 어머니 장례식을 치렀어요. 경험이 없다보니 어떻게 준비해야 할지 막막 했었는데 전화상담부터 너무 친절하게 설명도 잘 해주시고 장례식 당일날 전화만 하니 장례식장으로 오셔서 진행을 너무 잘 해주셨습니다. 어머니 관에 꽃장식도 너무 예쁘고 마음에 들었습니다. 어머니 살아 생전 꽃을 좋아하셔서 정말 꽃길상조 선택하길 잘 했다는 칭찬도 친척분들에게 들었어요. 
           </p>
           <a
@@ -40,31 +40,39 @@ export function MainReview() {
             </svg>
           </a>
         </div>
-
-        <div
-          className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800"
-          id="statistics"
-          role="tabpanel"
-          aria-labelledby="statistics-tab"
-        >
-          <dl className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
-            <div className="flex flex-col">
-              <dt className="mb-2 text-3xl font-extrabold">73M+</dt>
-              <dd className="text-gray-500 dark:text-gray-400">Developers</dd>
-            </div>
-            <div className="flex flex-col">
-              <dt className="mb-2 text-3xl font-extrabold">100M+</dt>
-              <dd className="text-gray-500 dark:text-gray-400">Public repositories</dd>
-            </div>
-            <div className="flex flex-col">
-              <dt className="mb-2 text-3xl font-extrabold">1000s</dt>
-              <dd className="text-gray-500 dark:text-gray-400">Open source projects</dd>
-            </div>
-          </dl>
-        </div>
       </div>
       <div className="Tab:w-2/3">
         <ul className="flex flex-col Tab:flex-row">
+          <li className="relative w-full h-64 Tab:pt-14 sm:w-64 sm:h-fit Tab:p-2">
+            <Image
+              src="/img/main/review_img1.jpg"
+              alt="Description"
+              // layout="fill"
+              className="object-cover"
+              width={300}
+              height={300}
+            />
+          </li>
+          <li className="relative w-full h-64 Tab:pt-14 sm:w-64 sm:h-fit Tab:p-2">
+            <Image
+              src="/img/main/review_img2.jpg"
+              alt="Description"
+              className="object-cover"
+              width={300}
+              height={300}
+            />
+          </li>
+          <li className="relative w-full h-64 Tab:pt-14 sm:w-64 sm:h-fit Tab:p-2">
+            <Image
+              src="/img/main/review_img3.jpg"
+              alt="Description"
+              className="object-cover"
+              width={300}
+              height={300}
+            />
+          </li>
+        </ul>
+        {/* <ul className="flex flex-col Tab:flex-row">
           <li>
             <Image src="/img/main/review_img1.jpg" alt="Description" layout="responsive" width={200} height={200} />
           </li>
@@ -74,7 +82,7 @@ export function MainReview() {
           <li>
             <Image src="/img/main/review_img3.jpg" alt="Description" layout="responsive" width={200} height={200} />
           </li>
-        </ul>
+        </ul> */}
       </div>
     </div>
   )
