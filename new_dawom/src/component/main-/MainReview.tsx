799ce74fa4f9bc9f7f54927ef3bc9bcb569f8605
yeowool1010 <yeowool1010@github.com>
2025-07-23 14:06:06 +0900

    빌드에러 해결

--- new_dawom/src/component/main-/MainReview.tsx
+++ new_dawom/src/component/main-/MainReview.tsx
@@ -33,9 +33,9 @@ export const MainReview = () => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m1 9 4-4-4-4"
               />
             </svg>
